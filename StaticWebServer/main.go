a1dfbced54701f206d9f63752d3b42257d838d85
QuickOrBeDead <boraakgn@gmail.com>
2022-11-06 16:08:54 +0300

    add css handler add html template

--- StaticWebServer/main.go
+++ StaticWebServer/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
+	"path"
 	"regexp"
 	"strconv"
 )
@@ -14,22 +17,49 @@ type Page struct {
 	Body []byte
 }
 
-var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
+var validPagePath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
 
-func loadPage(name string) (*Page, error) {
-	path := "Pages/" + name + ".html"
-	if _, err := os.Stat(path); err == nil {
-		body, err := os.ReadFile(path)
+func loadFile(name string) ([]byte, error) {
+	if _, err := os.Stat(name); err == nil {
+		body, err := os.ReadFile(name)
 		if err != nil {
 			return nil, err
 		}
 
-		return &Page{Name: name, Body: body}, nil
+		return body, nil
 	} else if errors.Is(err, os.ErrNotExist) {
 		return nil, nil
 	} else {
 		return nil, err
 	}
+}
+
+func loadPage(name string) (*Page, error) {
+	body, err := loadFile(path.Join("Pages", fmt.Sprintf("%s.html", name)))
+	if err != nil {
+		return nil, err
+	} else if body == nil {
+		return nil, nil
+	}
+
+	return &Page{Name: name, Body: body}, nil
+}
+
+func loadData(name string) (any, error) {
+	content, err := loadFile(path.Join("Pages", fmt.Sprintf("%s.json", name)))
+	if err != nil {
+		return nil, err
+	} else if content == nil {
+		return nil, nil
+	}
+
+	var data any
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 func main() {
@@ -46,24 +76,64 @@ func main() {
 	}
 
 	http.HandleFunc("/", pageHandler)
+	http.HandleFunc("/css/", cssHandler)
 	err = http.ListenAndServe(":"+strconv.Itoa(port), nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func pageHandler(res http.ResponseWriter, req *http.Request) {
-	pageName := validPath.FindStringSubmatch(req.URL.Path)
-	if pageName == nil {
+func cssHandler(res http.ResponseWriter, req *http.Request) {
+	var cssName = req.URL.Path[len("/css/"):]
+	if cssName == "" {
 		res.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	page, err := loadPage(req.URL.Path[1:])
+	content, err := loadFile(path.Join("Pages", "css", cssName))
+	if err != nil {
+		http.Error(res, "css load error", http.StatusInternalServerError)
+	} else if content == nil {
+		http.NotFound(res, req)
+	}
+
+	res.Header().Set("Content-Type", "text/css; charset=utf-8")
+	res.Write(content)
+}
+
+func pageHandler(res http.ResponseWriter, req *http.Request) {
+	pageNameMatch := validPagePath.FindStringSubmatch(req.URL.Path)
+	if pageNameMatch == nil {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pageName := pageNameMatch[1]
+	page, err := loadPage(pageName)
 	if err != nil {
 		http.Error(res, "page load error", http.StatusInternalServerError)
 	} else if page == nil {
 		http.NotFound(res, req)
+	}
+
+	data, err := loadData(pageName)
+	if err != nil {
+		http.Error(res, "page data load error", http.StatusInternalServerError)
+		return
+	}
+
+	if data != nil {
+		t, err := template.New("t").Parse(string(page.Body))
+		if err != nil {
+			http.Error(res, "page template parse error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = t.Execute(res, data)
+		if err != nil {
+			http.Error(res, "page template execute error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 	} else {
 		res.Write(page.Body)
 	}
