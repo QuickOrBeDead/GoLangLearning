d90795b6617205dffe2ff5319726c63236f13095
QuickOrBeDead <boraakgn@gmail.com>
2022-11-10 17:50:30 +0300

    add Link struct

--- HtmlLinkParser/main.go
+++ HtmlLinkParser/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
+
+type Link struct {
+	Text string
+	Href string
+}
 
 func main() {
 	file, _ := os.Open("test.html")
@@ -16,12 +22,18 @@ func main() {
 		panic(err)
 	}
 
+	links := make([]Link, 0, 10)
 	var searchLinks func(*html.Node)
 	searchLinks = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, attr := range n.Attr {
 				if attr.Key == "href" {
-					fmt.Println(attr.Val)
+					var text string
+					if n.FirstChild != nil {
+						text = getText(n.FirstChild)
+					}
+					links = append(links, Link{Href: attr.Val, Text: text})
+					break
 				}
 			}
 		}
@@ -32,4 +44,30 @@ func main() {
 	}
 
 	searchLinks(rootNode)
+
+	fmt.Println(links)
+}
+
+func getText(n *html.Node) string {
+	var sb strings.Builder
+	var t func(n *html.Node)
+	t = func(n *html.Node) {
+		if n.Type == html.TextNode {
+			text := strings.Join(strings.Fields(n.Data), " ")
+			sb.WriteString(text)
+			sb.WriteRune(' ')
+		}
+
+		for child := n.FirstChild; child != nil; child = child.NextSibling {
+			t(child)
+		}
+
+		if n.NextSibling != nil {
+			t(n.NextSibling)
+		}
+	}
+
+	t(n)
+
+	return strings.Trim(sb.String(), " ")
 }
