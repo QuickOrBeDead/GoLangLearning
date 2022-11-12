0d9449c1157f44d3ffe49faf6b9f4d2bd894c345
QuickOrBeDead <boraakgn@gmail.com>
2022-11-12 23:17:04 +0300

    add site.css and handle static files via httpFileServer

--- ChooseYourOwnAdventure/main.go
+++ ChooseYourOwnAdventure/main.go
@@ -26,6 +26,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", storyHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
 
